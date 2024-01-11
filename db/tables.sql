3bfdf4ee1b220c11c2346968c2bbedb8af1b112a
t06112003 <130381074+t06112003@users.noreply.github.com>
2024-01-11 19:48:01 +0700

    ád

---
+++ db/tables.sql
@@ -0,0 +1,14 @@
+--USE database
+DROP DATABASE BTL;
+
+CREATE DATABASE BTL;
+
+USE BTL;
+
+CREATE TABLE "User" (
+    UserId bigint IDENTITY(1,1) PRIMARY KEY,
+    Username varchar(16) NOT NULL UNIQUE CHECK (LEN(Username) >= 8),
+    Password varchar(16) NOT NULL CHECK (LEN(Password) >= 8),
+    IsAdmin bit NOT NULL,
+    AvatarUrl nvarchar(300) NOT NULL,
+);
