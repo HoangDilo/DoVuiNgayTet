d60163db14e635e7194ec34ddf5a78d6f6c84cc2
t06112003 <130381074+t06112003@users.noreply.github.com>
2024-01-11 19:53:33 +0700

    asd

---
+++ db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO "User"(Username, Password, IsAdmin, AvatarUrl) VALUES ('hoenghoeng', 'hoenghoeng', True, 'https://res.cloudinary.com/dslzbnfu8/image/upload/v1699185130/samples/DuRiu.png');
+INSERT INTO "User"(Username, Password, IsAdmin, AvatarUrl) VALUES ('thengtheng', 'thengtheng', True, 'https://res.cloudinary.com/dslzbnfu8/image/upload/v1699185130/samples/DuRiu.png');
+INSERT INTO "User"(Username, Password, IsAdmin, AvatarUrl) VALUES ('juanjuan', 'juanjuan', True, 'https://res.cloudinary.com/dslzbnfu8/image/upload/v1699185130/samples/DuRiu.png');
