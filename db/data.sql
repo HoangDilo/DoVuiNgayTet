99388745f03f8b8bf1aad14eecaa14d5a309e9a6
t06112003 <130381074+t06112003@users.noreply.github.com>
2024-01-11 21:11:19 +0700

    sua sua

---
+++ db/data.sql
@@ -0,0 +1,5 @@
+Use BTL
+
+INSERT INTO "User"(Username, Password, IsAdmin) VALUES ('hoenghoeng', 'hoenghoeng', 'True');
+INSERT INTO "User"(Username, Password, IsAdmin) VALUES ('thengtheng', 'thengtheng', 'True');
+INSERT INTO "User"(Username, Password, IsAdmin) VALUES ('juanjuan', 'juanjuan', 'True');
--- db/tables.sql
+++ db/tables.sql
@@ -10,5 +10,18 @@ CREATE TABLE "User" (
     Username varchar(16) NOT NULL UNIQUE CHECK (LEN(Username) >= 8),
     Password varchar(16) NOT NULL CHECK (LEN(Password) >= 8),
     IsAdmin bit NOT NULL,
-    AvatarUrl nvarchar(300) NOT NULL,
+    Link varchar(255),
+    Money int,
+);
+
+CREATE TABLE Questions (
+    QuestionId bigint IDENTITY(1,1) PRIMARY KEY,
+    Quest nvarchar(255) NOT NULL,
+);
+
+CREATE TABLE Answers (
+    AnswerId bigint IDENTITY(1,1) PRIMARY KEY,
+    QuestionId bigint NOT NULL FOREIGN KEY REFERENCES Questions(QuestionId),
+    Answer nvarchar(255) NOT NULL,
+    IsCorrect bit NOT NULL,
 );
