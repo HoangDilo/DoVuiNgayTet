8c5be7be3f33e05d0677cc19acea558fa0620a85
LeeHaii <thangthcskt@gmail.com>
2024-01-16 16:15:05 +0700

    update 1 ti
    abc

---
+++ fe/src/Input/Input.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, KeyboardEvent } from "react";
+import './Input.css'
+
+function Input({ value, label, type, icon, setData, onSubmit }: { value: string, label: string, type: string, icon: string, setData: (data: string) => void, onSubmit: () => void }) {
+    const [internalType, setInternalType] = useState("")
+    const [inputValue, setInputValue] = useState("")
+    const [internalErrorMessage, setInternalErrorMessage] = useState("")
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key == "Enter") {
+            onSubmit();
+        }
+    }
+
+    useEffect(() => {
+        setInternalType(type);
+    }, [type])
+
+    useEffect(() => {
+        setData(inputValue);
+    }, [inputValue])
+
+    useEffect(() => {
+        if (value) {
+            setInputValue(value)
+        }
+    }, [value])
+
+    return (
+        <>
+            <div className="Input-container">
+                <div className={`label-${label}`}>{label}</div>
+                <div className={`${icon}`}></div>
+                <input 
+                    type={internalType}
+                    id={`input-${label}`}
+                    className={`${icon && 'isIcon'} is${type}`}
+                    placeholder={label}
+                    onChange={(event) => setData(event.target.value)}
+                    onKeyDown={(event) => handleKeyDown(event)}/>
+            </div>
+        </>
+    )
+}
+
+export default Input
--- fe/src/Login/Login.tsx
+++ fe/src/Login/Login.tsx
@@ -1,10 +1,44 @@
 import './Login.scss'
+import { useState } from 'react';
+import Input from '../Input/Input'
 
-function Login(){
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    function handleSubmit() {
 
-    return(
+    }
+
+    return (
         <>
-            <div>hello</div>
+
+            <div className="Login-background">
+                <div className="Login-roll">
+                    <div className="Login-head-roll"></div>
+                    <div className="Login-container">
+                        <div className="Login-text">Login</div>
+                        <Input
+                            value={username}
+                            label='username'
+                            type='text'
+                            icon=''
+                            setData={setUsername}
+                            onSubmit={handleSubmit}
+                        />
+                        <Input
+                            value={password}
+                            label='password'
+                            type='password'
+                            icon=''
+                            setData={setUsername}
+                            onSubmit={handleSubmit}
+                        />
+                        <div className="Login-forgot-pass">forgot password?</div>
+                        <button className='Login-submit'>Login</button>
+                    </div>
+                    <div className="Login-bottom-roll"></div>
+                </div>
+            </div>
         </>
     )
 }
