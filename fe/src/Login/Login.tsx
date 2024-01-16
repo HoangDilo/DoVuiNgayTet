fe0f6d8cad9b147898dc45df5baf4e6adc21feba
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2024-01-16 23:45:49 +0700

    add open roll

--- fe/src/Login/Login.tsx
+++ fe/src/Login/Login.tsx
@@ -1,57 +1,71 @@
-import './Login.scss'
-import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import Input from '../Input/Input'
+import "./Login.scss";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../Input/Input";
 
 function Login() {
-    const navigate = useNavigate()
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [isMounted, setIsMounted] = useState(false)
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
 
-    const handleSubmit = () => {}
+  const handleSubmit = () => {};
 
-    const handleNavigateSignUp = () => {
-        navigate('/signup')
+  const handleNavigateSignUp = () => {
+    navigate("/signup");
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+        setIsMounted(true);
+    }, 750);
+
+    return () => {
+        clearTimeout(timeout)
     }
+  }, []);
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
-
-    return (
-        <>
-
-            <div className="Login-background">
-                <div className={`Login-roll ${!isMounted ? 'Login-roll-close' : ''}`}>
-                    <div className="Login-head-roll"></div>
-                    <div className="Login-container">
-                        <div className="Login-text">Login</div>
-                        <Input
-                            value={username}
-                            label='username'
-                            type='text'
-                            icon=''
-                            setData={setUsername}
-                            onSubmit={handleSubmit}
-                        />
-                        <Input
-                            value={password}
-                            label='password'
-                            type='password'
-                            icon=''
-                            setData={setPassword}
-                            onSubmit={handleSubmit}
-                        />
-                        <div className="Login-forgot-pass">forgot password?</div>
-                        <button className='Login-submit'>Login</button>
-                        <div className="Login-to-signup">Dont have an account? <span onClick={handleNavigateSignUp}>Create one here!</span></div>
-                    </div>
-                    <div className="Login-bottom-roll"></div>
-                </div>
+  return (
+    <>
+      <div className="Login-background">
+        <div className={`Login-roll`}>
+          <div className="Login-container-wrapper">
+            <div className="Login-head-roll"></div>
+            <div
+              className={`Login-container ${
+                !isMounted ? "Login-roll-close" : "Login-roll-open"
+              }`}
+            >
+              <div className="Login-text">Login</div>
+              <Input
+                value={username}
+                label="username"
+                type="text"
+                icon=""
+                setData={setUsername}
+                onSubmit={handleSubmit}
+              />
+              <Input
+                value={password}
+                label="password"
+                type="password"
+                icon=""
+                setData={setPassword}
+                onSubmit={handleSubmit}
+              />
+              <div className="Login-forgot-pass">forgot password?</div>
+              <button className="Login-submit">Login</button>
+              <div className="Login-to-signup">
+                Dont have an account?{" "}
+                <span onClick={handleNavigateSignUp}>Create one here!</span>
+              </div>
             </div>
-        </>
-    )
+            <div className="Login-bottom-roll"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
