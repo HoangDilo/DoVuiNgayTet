70492c3fb4101130f1dd1b73a9f608ecfb39c72e
LeeHaii <thangthcskt@gmail.com>
2024-01-17 00:02:22 +0700

    sech2
    sasd

--- fe/src/SignUp/SignUp.tsx
+++ fe/src/SignUp/SignUp.tsx
@@ -4,56 +4,73 @@ import { Link, useNavigate } from 'react-router-dom'
 import Input from '../Input/Input'
 
 function SignUp() {
-    const navigate = useNavigate()
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [repeatPassword, setRepeatPassword] = useState("")
+    const [isMounted, setIsMounted] = useState(false);
 
-    const handleSubmit = () => {}
+    const handleSubmit = () => { };
 
     const handleNavigateLogin = () => {
-        navigate('/login')
-    }
+        setIsMounted(false)
+        setTimeout(() => {
+            navigate("/login");
+        }, 500);
+    };
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsMounted(true);
+        }, 750);
+
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, []);
 
     return (
         <>
             <div className="Login-background">
-                <div className="Login-roll">
-                    <div className="Login-head-roll"></div>
-                    <div className="Login-container">
-                        <div className="Login-text">Sign Up</div>
-                        <Input
-                            value={username}
-                            label='username'
-                            type='text'
-                            icon=''
-                            setData={setUsername}
-                            onSubmit={handleSubmit}
-                        />
-                        <Input
-                            value={password}
-                            label='password'
-                            type='password'
-                            icon=''
-                            setData={setPassword}
-                            onSubmit={handleSubmit}
-                        />
-                        <Input
-                            value={repeatPassword}
-                            label='repeat password'
-                            type='password'
-                            icon=''
-                            setData={setRepeatPassword}
-                            onSubmit={handleSubmit}
-                        />
-                        <button className='Login-submit'>Sign Up</button>
-                        <div className="Login-to-signup">Already had an account? <span onClick={handleNavigateLogin}>Login here!</span></div>
+                <div className={`Login-roll`}>
+                    <div className="Login-container-wrapper">
+                        <div className="Login-head-roll"></div>
+                        <div
+                            className={`Login-container ${!isMounted ? "Sign-up-roll-close" : "Sign-up-roll-open"}`}>
+                            <div className="Login-text">Sign Up</div>
+                            <Input
+                                value={username}
+                                label="username"
+                                type="text"
+                                icon=""
+                                setData={setUsername}
+                                onSubmit={handleSubmit}
+                            />
+                            <Input
+                                value={password}
+                                label="password"
+                                type="password"
+                                icon=""
+                                setData={setPassword}
+                                onSubmit={handleSubmit}
+                            />
+                            <Input
+                                value={repeatPassword}
+                                label='repeat password'
+                                type='password'
+                                icon=''
+                                setData={setRepeatPassword}
+                                onSubmit={handleSubmit}
+                            />
+                            <button className='Login-submit'>Sign Up</button>
+                            <div className="Login-to-signup">Already had an account? <span onClick={handleNavigateLogin}>Login here!</span></div>
+                        </div>
+                        <div className="Login-bottom-roll"></div>
                     </div>
-                    <div className="Login-bottom-roll"></div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default SignUp
