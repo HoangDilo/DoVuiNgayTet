043b1cb2715335c2e1a24cea3938cc448a35113d
t06112003 <130381074+t06112003@users.noreply.github.com>
2024-01-23 10:54:35 +0700

    asdasd

--- be/Model/Dto/AnswerListOutputDto.cs
+++ be/Model/Dto/AnswerListOutputDto.cs
@@ -10,6 +10,5 @@ namespace be.Model.Dto
         public long AnswerId { get; set; }
         public string AnswerText { get; set; }
         public bool IsCorrect { get; set; }
-        public string QuestionText { get; set; }
     }
 }
