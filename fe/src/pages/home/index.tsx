2c825ad6ebfdefe490ae3de7d7c5e534103e4742
LeeHaii <thangthcskt@gmail.com>
2024-01-26 01:35:29 +0700

    mockup cau do
    sehc sehc nhieu loi o console qua ma toi buon ngu

--- fe/src/pages/home/index.tsx
+++ fe/src/pages/home/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import './HomePage.scss'
+import Button from '../../components/common/Button/Button'
 
 export default function HomePage() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsMounted(true);
+    }, 750);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+  
+  const handleSubmit = () => {}
   return (
-    <div>HomePage</div>
+    <div className="home-background">
+      <div className="home-roll">
+        <div className="home-question-container-wrapper">
+          <div className="home-left-roll"></div>
+          <div className={`home-question-container ${ !isMounted ? "home-roll-close" : "home-roll-open"}`}></div>
+          <div className="home-right-roll"></div>
+        </div>
+      </div>
+      <div className="home-answers">
+      <Button label={``} type="chit" onSubmit={handleSubmit}/>
+      <Button label={``} type="chit" onSubmit={handleSubmit}/>
+      <Button label={``} type="chit" onSubmit={handleSubmit}/>
+      <Button label={``} type="chit" onSubmit={handleSubmit}/>
+      </div>
+    </div>
   )
 }
