db020b08f49b7ad0ea4b2902d9036c46506f6073
LeeHaii <thangthcskt@gmail.com>
2024-02-21 01:04:28 +0700

    bucac cho
    adwadwad

--- fe/src/pages/lixi/lixi.tsx
+++ fe/src/pages/lixi/lixi.tsx
@@ -3,14 +3,42 @@ import LixiItem from "./lixi-item/lixi-item";
 import "./lixi.scss";
 
 export default function Lixi() {
-  const [lixi, setLixi] = useState<{ id: number; x: number }[]>([]);
+  const [lixi, setLixi] = useState<
+    {
+      id: number;
+      x: number;
+      lixiIndex: number;
+      size: number;
+      direction: boolean;
+    }[]
+  >([]);
   const isGenerating = useRef(true);
-  
 
   const generateRandomX = () => Math.random() * window.innerWidth;
 
+  const randomLixi = () => {
+    return Math.floor(Math.random() * 6);
+  };
+
+  const randomSize = () => {
+    return Math.floor(Math.random() * 20) + 40;
+  };
+
+  const randomDirection = () => {
+    return !!Math.floor(Math.random()*2)
+  }
+
   const generateNewLixi = () => {
-    const newLixi = [...lixi, { id: Date.now(), x: generateRandomX() }];
+    const newLixi = [
+      ...lixi,
+      {
+        id: Date.now(),
+        x: generateRandomX(),
+        lixiIndex: randomLixi(),
+        size: randomSize(),
+        direction: randomDirection()
+      },
+    ];
     setLixi(newLixi);
   };
 
@@ -31,12 +59,15 @@ export default function Lixi() {
 
   return (
     <div className="lixi-background">
-      {lixi.map(({ id, x }) => (
+      {lixi.map(({ id, x, lixiIndex, size, direction }) => (
         <LixiItem
           key={id}
           x={x}
           onClick={() => {}}
           onTransitionEnd={() => handleDeleteLixi(id)}
+          lixiIndex={lixiIndex}
+          size={size}
+          direction={direction}
         />
       ))}
     </div>
