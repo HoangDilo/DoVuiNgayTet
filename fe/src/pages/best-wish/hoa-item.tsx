ca01deba285b2e1ba9c324083f46a1b84c1e28c6
LeeHaii <thangthcskt@gmail.com>
2024-02-28 01:19:08 +0700

    bucac r
    dawidjwafd

---
+++ fe/src/pages/best-wish/hoa-item.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import "./BestWish.scss";
+import hoaTexture from "@/assets/hoa.png";
+
+interface HoaItemProps {
+    x: number;
+    onTransitionEnd: () => void;
+}
+
+export default function HoaItem(props: HoaItemProps) {
+    const {x, onTransitionEnd} = props
+    const [isFalling, setIsFalling] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+          setIsFalling(true)
+        }, 1000)
+      }, [])
+
+  return (
+    <>
+      <div
+        className={`hoa-item ${isFalling && "fall"}`}
+        style={{ left: x }}
+        onTransitionEnd={onTransitionEnd}
+      >
+        <img className="hoa-texture" src={hoaTexture} alt="sehc no cac" />
+      </div>
+    </>
+  );
+}
--- fe/src/pages/best-wish/index.tsx
+++ fe/src/pages/best-wish/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./BestWish.scss";
+import HoaItem from "./hoa-item";
 
 export default function BestWishPage() {
+  const [hoa, setHoa] = useState<
+    {
+      id: number;
+      x: number;
+    }[]
+  >([]);
+
+  const generateRandomX = () => Math.random() * window.innerWidth;
+
+  const generateNewHoa = () => {
+    const newHoa = [
+      ...hoa,
+      {
+        id: Date.now(),
+        x: generateRandomX(),
+      },
+    ];
+    setHoa(newHoa);
+  };
+
+  const handleDeleteHoa = (id: number) => {
+    const updatedHoa = hoa.filter((item) => item.id !== id);
+    setHoa(updatedHoa);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      generateNewHoa();
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [hoa]);
+
   return (
-    <div>BestWishPage</div>
-  )
+    <div className="best-wish-background">
+      <div className="best-wish-text">
+        <div className="best-wish-text-top">sech sech sech</div>
+        <div className="best-wish-text-mid">skibidi dom dom yes yes</div>
+        <div className="best-wish-text-bot">sech sech sech</div>
+      </div>
+      <div className="hoa-roi">
+        <div className="hoa">
+          {hoa.map(({ id, x }) => (
+            <HoaItem
+              x={x}
+              key={id}
+              onTransitionEnd={() => handleDeleteHoa(id)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
